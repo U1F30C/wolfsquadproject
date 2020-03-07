84ac64ac9c21f72b933adefd90b9d1d9c319d785
SanjeVicor <santiago.villalobos@alumnos.udg.mx>
2020-03-06 23:31:23 -0600

    admin login, schools wireframes, admin account settings, partials

--- start/routes.js
+++ start/routes.js
@@ -17,3 +17,21 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome')
+Route.on('/about').render('about')
+Route.on('/professor').render('professor')
+Route.on('/student').render('student')
+Route.on('/contact').render('contact')
+
+/* 
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+*/
+Route.on('/login').render('log-in')
+Route.on('/recover_password').render('recover-password')
+Route.on('/new_password').render('new-password')
+Route.on('/new_email').render('new-email')
+
+Route.on('/schools').render('create-schools-form')
+Route.on('/automatic-school-form').render('school-automatic-form')
+Route.on('/manual-school-form').render('school-manual-form')
