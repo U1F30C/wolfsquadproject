94862cf19006b04701f2188059b654fa7902cb6c
Eric <eric.lopezs@alumnos.udg.mx>
2020-03-28 17:07:38 -0600

    Remove renamed scenes

--- start/routes.js
+++ start/routes.js
@@ -18,19 +18,14 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 Route.on('/about').render('about')
-Route.on('/professor').render('professor')
-Route.on('/student').render('student')
 Route.on('/contact').render('contact')
+
 /* 
 |--------------------------------------------------------------------------
 | Admin
 |--------------------------------------------------------------------------
 */
-Route.on('/login').render('login')
-
-Route.on('/infoescuela').render('infoescuela')
-Route.on('/infosalon').render('infosalon')
-
+Route.on('/login').render('log-in')
 Route.on('/recover_password').render('recover-password')
 Route.on('/new_password').render('new-password')
 Route.on('/new_email').render('new-email')
@@ -38,3 +33,23 @@ Route.on('/new_email').render('new-email')
 Route.on('/schools').render('create-schools-form')
 Route.on('/automatic-school-form').render('school-automatic-form')
 Route.on('/manual-school-form').render('school-manual-form')
+
+Route.on('/statistics').render('school-info')
+Route.on('/statistics-classroom').render('classroom-info')
+
+/* 
+|--------------------------------------------------------------------------
+| Teacher
+|--------------------------------------------------------------------------
+*/
+Route.on('/professor').render('professor')
+Route.on('/professor-list').render('professor-list')
+
+/* 
+|--------------------------------------------------------------------------
+| Student
+|--------------------------------------------------------------------------
+*/
+Route.on('/student').render('student')
+Route.on('/student-questionnarie').render('questionnaire')
+Route.on('/student-questionnarie-end').render('end-questionnaire')
