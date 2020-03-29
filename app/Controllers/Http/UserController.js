eae24aa44954dc2d75ba33003eebd7d92028be2c
Eric <eric.lopezs@alumnos.udg.mx>
2020-03-29 16:50:01 -0600

    Remove console.log

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,7 +5,6 @@ class UserController {
     const { email, password } = request.all();
     try {
       await auth.attempt(email, password);
-      console.log("hola muy buenas a todos");
       response.route("schoolsManagement");
     } catch {
       response.route("login");
