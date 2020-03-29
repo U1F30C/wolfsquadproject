c0121010b090a88c2f7913e120b985c791cce5a1
Eric <eric.lopezs@alumnos.udg.mx>
2020-03-29 01:59:57 -0600

    Add login controller and auth middleware

--- start/routes.js
+++ start/routes.js
@@ -17,6 +17,7 @@
 const Route = use('Route')
 
 Route.on('/').render('welcome').as('welcome')
+Route.post('/login', 'UserController.login')
 Route.on('/about').render('about').as('about')
 Route.on('/contact').render('contact').as('contact')
 
@@ -25,17 +26,17 @@ Route.on('/contact').render('contact').as('contact')
 | Admin
 |--------------------------------------------------------------------------
 */
-Route.on('/login').render('log-in').as('login')
-Route.on('/recover_password').render('recover-password').as('passwordRecovery')
-Route.on('/new_password').render('new-password').as('passwordChange')
-Route.on('/new_email').render('new-email').as('emailChange')
+Route.on('/login').render('log-in').as('login').middleware(['guest'])
+Route.on('/recover_password').render('recover-password').as('passwordRecovery').middleware(['guest'])
+Route.on('/new_password').render('new-password').as('passwordChange').middleware(['auth'])
+Route.on('/new_email').render('new-email').as('emailChange').middleware(['auth'])
 
-Route.on('/schools').render('create-schools-form').as('schoolsManagement')
-Route.on('/automatic-school-form').render('school-automatic-form').as('automaticForm')
-Route.on('/manual-school-form').render('school-manual-form').as('manualForm')
+Route.on('/schools').render('create-schools-form').as('schoolsManagement').middleware(['auth'])
+Route.on('/automatic-school-form').render('school-automatic-form').as('automaticForm').middleware(['auth'])
+Route.on('/manual-school-form').render('school-manual-form').as('manualForm').middleware(['auth'])
 
-Route.on('/statistics').render('school-info').as('statistics')
-Route.on('/statistics-classroom').render('classroom-info').as('classRoomAnswers')
+Route.on('/statistics').render('school-info').as('statistics').middleware(['auth'])
+Route.on('/statistics-classroom').render('classroom-info').as('classRoomAnswers').middleware(['auth'])
 
 /* 
 |--------------------------------------------------------------------------
