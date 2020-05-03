32ac35fd6571b6ffc6c5f3cfcbcb93767a041d23
Eric <eric.lopezs@alumnos.udg.mx>
2020-05-02 19:40:12 -0500

    Clean up seeders

--- database/seeds/AreaSeeder.js
+++ database/seeds/AreaSeeder.js
@@ -14,45 +14,25 @@
 const Factory = use('Factory');
 const Database = use('Database');
 
+const areaNames = [
+  'Uso/abuso de sustancias',
+  'Salud mental',
+  'Relaciones Familiares',
+  'Relaciones con amigos',
+  'Nivel educativo',
+  'Interés laboral',
+  'Conducta agresiva/delictiva',
+];
+
 class AreaSeeder {
   async run() {
-    const areas = await Database.table('areas');
-    console.log(areas);
-
-    const area_1 = await Factory.model('App/Models/Area').create({
-      name: 'Uso/abuso de sustancias',
-    });
-    Database.table('areas').insert(area_1);
-
-    const area_2 = await Factory.model('App/Models/Area').create({
-      name: 'Salud mental',
-    });
-    Database.table('areas').insert(area_2);
-
-    const area_3 = await Factory.model('App/Models/Area').create({
-      name: 'Relaciones Familiares',
-    });
-    Database.table('areas').insert(area_3);
-
-    const area_4 = await Factory.model('App/Models/Area').create({
-      name: 'Relaciones con amigos',
-    });
-    Database.table('areas').insert(area_4);
-
-    const area_5 = await Factory.model('App/Models/Area').create({
-      name: 'Nivel educativo',
-    });
-    Database.table('areas').insert(area_5);
-
-    const area_6 = await Factory.model('App/Models/Area').create({
-      name: 'Interés laboral',
-    });
-    Database.table('areas').insert(area_6);
-
-    const area_7 = await Factory.model('App/Models/Area').create({
-      name: 'Conducta agresiva/delictiva',
-    });
-    Database.table('areas').insert(area_7);
+    let areas = areaNames.map(
+      async (area) =>
+        await Factory.model('App/Models/Area').create({
+          name: area,
+        })
+    );
+    Database.table('areas').insert(areas);
   }
 }
 
