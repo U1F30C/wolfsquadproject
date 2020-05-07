7f08844d52212e1ceb727c88ffe158b89d2e71a5
Eric <eric.lopezs@alumnos.udg.mx>
2020-05-07 00:08:55 -0500

    Separate specific seeding data

---
+++ database/data/areas.js
@@ -0,0 +1,12 @@
+
+const areaNames = [
+    'Uso/abuso de sustancias',
+    'Salud mental',
+    'Relaciones Familiares',
+    'Relaciones con amigos',
+    'Nivel educativo',
+    'Interés laboral',
+    'Conducta agresiva/delictiva',
+  ];
+
+module.exports = areaNames;
