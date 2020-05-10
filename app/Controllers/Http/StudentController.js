cbea77dcc6ffe82aaa4b73861fb0841e815e6b54
Eric <eric.lopezs@alumnos.udg.mx>
2020-05-10 11:53:32 -0500

    Forrmat student controller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -1,23 +1,22 @@
 'use strict';
- 
-const Student = use('App/Models/Student')
+
+const Student = use('App/Models/Student');
 
 class StudentController {
+  async delete({ request, response, params }) {
+    const id = request.input('id');
+    const student = await Student.find(id);
 
-    async delete({ request, response, params }){ 
-        const id = request.input('id');
-        const student = await Student.find(id)
-        
-        if (student){ 
-            await student.delete() 
-            response.redirect(request.header('referer'));
-        }else{
-            var error = {
-              msg: 'Registo no encontrado',
-            };
-            response.status(400).send(error);
-        }
+    if (student) {
+      await student.delete();
+      response.redirect(request.header('referer'));
+    } else {
+      var error = {
+        msg: 'Registo no encontrado',
+      };
+      response.status(400).send(error);
     }
+  }
 }
 
 module.exports = StudentController;
